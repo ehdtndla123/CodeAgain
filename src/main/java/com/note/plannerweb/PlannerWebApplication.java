38d2b319ee338a032196d98774539943fa7357e0
ehdtndla123 <ehdtndla123@naver.com>
2022-08-27 22:49:45 +0900

    start-project

---
+++ src/main/java/com/note/plannerweb/PlannerWebApplication.java
@@ -0,0 +1,13 @@
+package com.note.plannerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/note/plannerweb/PlannerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.plannerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
