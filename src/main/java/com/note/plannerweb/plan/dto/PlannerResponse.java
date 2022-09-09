0bdf0b506981a630cd528d31e3a158790674f452
ehdtndla123 <ehdtndla123@naver.com>
2022-09-09 19:49:34 +0900

    PlannerResponse modifed

--- src/main/java/com/note/plannerweb/plan/dto/PlannerResponse.java
+++ src/main/java/com/note/plannerweb/plan/dto/PlannerResponse.java
@@ -1,8 +1,11 @@
 package com.note.plannerweb.plan.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.note.plannerweb.plan.domain.Plan;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +17,9 @@ import java.util.List;
 public class PlannerResponse {
 
     private Long id;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime targetDate;
 
     private List<PlanResponse> plans = new ArrayList<>();
 }
