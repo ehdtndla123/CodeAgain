53a7ea5a3e6fe500739c297ac9bb17a4bfaf4c2c
ehdtndla123 <ehdtndla123@naver.com>
2022-09-13 00:22:48 +0900

    CI CD Test

--- src/main/java/com/note/plannerweb/SchedulerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.note.plannerweb;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@EnableScheduling
-@SpringBootApplication
-public class SchedulerApplication {
-}
