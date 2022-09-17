8738af4fa76791cc5b3d5cf4811ff91107963084
ehdtndla123 <ehdtndla123@naver.com>
2022-09-17 16:29:37 +0900

    CORS Error Update

--- src/main/java/com/note/plannerweb/config/WebConfig.java
+++ src/main/java/com/note/plannerweb/config/WebConfig.java
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("*")
+//                .allowCredentials(true);
+//    }
 }
